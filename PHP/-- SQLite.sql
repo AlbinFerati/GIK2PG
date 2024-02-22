72eb6ae7bd901df34aca7bbae7ad82fe7b4cf507
Sebastian <90455425+Nordstrom94@users.noreply.github.com>
2024-02-22 10:56:10 +0100

    Sparar ner allt i databas

--- PHP/-- SQLite.sql
+++ PHP/-- SQLite.sql
@@ -79,3 +79,44 @@ CREATE TABLE fordon (
 SELECT * FROM fordon;
 
 DROP TABLE fordon;
+
+CREATE TABLE IF NOT EXISTS din_tabell (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    fnamn TEXT,
+    enamn TEXT,
+    telnr TEXT,
+    mail TEXT,
+    styrka_höger_arm INTEGER,
+    styrka_vänster_arm INTEGER,
+    styrka_höger_ben INTEGER,
+    styrka_vänster_ben INTEGER,
+    styrka_höger_hand INTEGER,
+    styrka_vänster_hand INTEGER,
+    kryckor BOOLEAN,
+    rullator BOOLEAN,
+    rullstol BOOLEAN,
+    manuell_rullstol_hjälpmotor BOOLEAN,
+    elmoped BOOLEAN,
+    permobil BOOLEAN,
+    annat_hjälpmedel TEXT,
+    kryckor_till_fordon BOOLEAN,
+    rullator_till_fordon BOOLEAN,
+    rullstol_till_fordon BOOLEAN,
+    hjälpmotor_till_fordon BOOLEAN,
+    elmoped_till_fordon BOOLEAN,
+    permobil_till_fordon BOOLEAN,
+    anpassad_bil INTEGER,
+    forare_passagerare TEXT,
+    mindre_personbil INTEGER,
+    storre_personbil INTEGER,
+    suv INTEGER,
+    mindre_transportbil INTEGER,
+    amerikansk_golvsanktbil INTEGER,
+    minibuss INTEGER,
+    annat_fordon TEXT,
+    vet_ej INTEGER
+);
+
+DROP TABLE din_tabell;
+
+SELECT * FROM din_tabell;
