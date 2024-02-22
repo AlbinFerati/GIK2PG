361705abd4bf10137609cfe984f78341ec9bdfde
leeken4 <89828493+leeken4@users.noreply.github.com>
2024-02-22 10:26:20 +0100

    forrdon fix stuff noobpush

--- PHP/sparadatafordon.php
+++ PHP/sparadatafordon.php
@@ -6,16 +6,16 @@ $dbh = new PDO('sqlite:../anpassarna.db');
 $stmt = $dbh->prepare("INSERT INTO fordon (forare_passagerare, anpassad_bil, mindre_personbil, storre_personbil, suv, mindre_transportbil, amerikansk_golvsanktbil, minibuss, annat_fordon, vet_ej) 
                       VALUES (:forare_passagerare, :anpassad_bil, :mindre_personbil, :storre_personbil, :suv, :mindre_transportbil, :amerikansk_golvsanktbil, :minibuss, :annat_fordon, :vet_ej)
                       ON CONFLICT(id) DO UPDATE SET forare_passagerare = :forare_passagerare, anpassad_bil = :anpassad_bil, mindre_personbil = :mindre_personbil, storre_personbil = :storre_personbil, 
-                      suv = :suv, mindre_transportbil = :mindre_transportbil, amerikansk_golvsänktbil = :amerikansk_golvsänktbil, minibuss = :minibuss, annat_fordon = :annat_fordon, vet_ej = :vet_ej");
+                      suv = :suv, mindre_transportbil = :mindre_transportbil, amerikansk_golvsanktbil = :amerikansk_golvsanktbil, minibuss = :minibuss, annat_fordon = :annat_fordon, vet_ej = :vet_ej");
 
 // Hämta data från POST-formuläret
 $forare_passagerare = $_POST['forare_passagerare'];
-$anpassad_bil = isset($_POST['anpassad_bil']) ? $_POST['anpassad_bil'] : 0;
-$mindre_personbil = $_POST['mindre_personbil'];
-$storre_personbil = $_POST['storre_personbil'];
-$suv = $_POST['suv'];
-$mindre_transportbil = $_POST['mindre_transportbil'];
-$amerikansk_golvsanktbil = $_POST['amerikansk_golvsänktbil'];
+$anpassad_bil = $_POST['anpassad_bil'];
+$mindre_personbil = $_POST['mindreBil'];
+$storre_personbil = $_POST['störrePersonbil'];
+$suv = $_POST['SUV'];
+$mindre_transportbil = $_POST['mindreTransportbil'];
+$amerikansk_golvsanktbil = $_POST['amerikanskGolvsänktbil'];
 $minibuss = $_POST['minibuss'];
 $annat_fordon = $_POST['annat_fordon'];
 $vet_ej = isset($_POST['vet_ej']) ? 1 : 0;
